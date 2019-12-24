705db1378def72b804cc7e317a85fa2bf4f0afeb
Abdelrahman Kazamel <50080379+Kazamel@users.noreply.github.com>
2019-12-24 02:12:36 +0200

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// function initMap (){
+//     var location = { lat: 30.111453, lng: 31.314413};
+//     var map = new google.maps.Map(document.getElementById('map'),{
+//     ZOOM:4,
+//     center:location
+//     });
+//     var marker = new google.maps.Marker({
+//         position:location,
+//         map:map
+//     });
+// }
